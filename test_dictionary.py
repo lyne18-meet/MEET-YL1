86d0b0a72f9d34ac8c60e14970a045ab982ae7ac
lyne18-meet <lyneshweiki@gmail.com>
2016-11-29 20:00:53 +0200

    comment

---
+++ test_dictionary.py
@@ -0,0 +1,16 @@
+#i'm making a dictionary
+d=dict()
+d["key"]="hello"
+my_list=["hi","hola"]
+print(my_list[1])
+print(d["key"])
+x=400
+d[x]=42
+print(d[x])
+print(d)
+my_list.append("hallo")
+print(my_list)
+#d.append("error")
+d2={"daffi":"duck","tom":"cat","club":"penguine"}
+print(d2)
+print(d2["daffi"])
